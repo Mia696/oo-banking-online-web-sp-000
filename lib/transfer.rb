58a03ef94ffc7939a67492770e3d492a0ea1ac4f
Mia <tamiapaschall@gmail.com>
2020-01-21 06:48:00 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,6 @@ def execute_transaction
   if @sender.balance > @amount && @status == "pending"
     @sender.balance -= @amount
     @receiver.balance += @amount
-    @status = "complete"
   else
    @status = "rejected"
    return "Transaction rejected. Please check your account balance."
