320ec34282e4a72fb9d81f03a2bc5bf41bde9a10
Mia <tamiapaschall@gmail.com>
2020-01-21 06:16:47 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,9 +13,9 @@ class Transfer
 end
 
 def execute_transaction
-  if valid? && sender.balance > amount && self.status == "pending"
-    sender.withdrawl(self.amount)
-    receiver.deposit(self.amount)
+  if @sender.balance > @amount && @status == "pending"
+    @sender.balance -= @amount
+    @receiver.balance += @amount
     self.status = "complete"
   else
     self.status = "rejected"
