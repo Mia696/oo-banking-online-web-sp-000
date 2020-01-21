2e52a95f4ce0ccee04eaedfd06c845e53dc9f2a4
Mia <tamiapaschall@gmail.com>
2020-01-21 05:51:48 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,20 +25,16 @@ end
 
 def reverse_transfer 
   if valid? && receiver.balance > amount && self.status == "complete"
-    receiver.balance -= amount
-    sender.balance += amount
+    sender.deposit(self.amount)
+    receiver.withdrawl(self.amount)
     self.status = "reversed"
   else
     reject_transfer
-  end
-end
-
-def reject_transfer
   self.status = "rejected"
   "Transaction rejected. Please check your account balance."
  end
 end
-  
+
   
   
   
