67e862deb3a18895c16c8ef5fa153c6c44fd35f3
Mia <tamiapaschall@gmail.com>
2020-01-21 05:53:00 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,8 @@ end
 
 def execute_transaction
   if valid? && sender.balance > amount && self.status == "pending"
-   
+    sender.withdrawl(self.amount)
+    receiver.deposit(self.amount)
     self.status = "complete"
   else
     reject_transfer
