d83d44eb715f71f871824169a219d337598e40e2
Mia <tamiapaschall@gmail.com>
2020-01-21 05:32:22 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -15,13 +15,12 @@ end
 def display_balance
   "Your balance is $#{balance}."
 end 
-  
-  def close_account 
-    self.status = "closed"
-  end
-  
+
   def valid?
     balance > 0 && status == "open"
   end 
   
+  def close_account 
+    self.status = "closed"
+  end
 end
